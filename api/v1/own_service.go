b4e75251a27000290f10bfa03b0ee816d883cc90
fu <shen.ren.love@163.com>
2020-08-30 20:00:56 +0800

    controller

--- api/v1/own_service.go
+++ api/v1/own_service.go
@@ -1,8 +1,22 @@
 package v1
 
 import (
+	"context"
+	"fmt"
+	"github.com/go-logr/logr"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/portsecurity"
+	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"net"
+	"reflect"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"time"
 )
 
 type ServicePort struct {
@@ -21,4 +35,147 @@ type OwnService struct {
 type ServicePortStatus struct {
 	corev1.ServicePort `json:"servicePort,omitempty"`
 	Health             bool `json:"health,omitempty"`
+}
+
+func (ownService *OwnService) MakeOwnResource(instance *Unit, logger logr.Logger,
+	scheme *runtime.Scheme) (interface{}, error) {
+
+	// new a Service object
+	svc := &corev1.Service{
+		// metadata field inherited from owner Unit
+		ObjectMeta: metav1.ObjectMeta{Name: instance.Name, Namespace: instance.Namespace, Labels: instance.Labels},
+		Spec: corev1.ServiceSpec{
+			Ports: ownService.Ports,
+			Type:  corev1.ServiceTypeClusterIP,
+		},
+	}
+
+	if ownService.ClusterIP != "" {
+		svc.Spec.ClusterIP = ownService.ClusterIP
+	}
+
+	// add selector
+	labelMap := make(map[string]string, 1)
+	labelMap["app"] = instance.Name
+	svc.Spec.Selector = labelMap
+
+	//svc.Spec.Selector =
+	// add ControllerReference for sts，the owner is Unit object
+	if err := controllerutil.SetControllerReference(instance, svc, scheme); err != nil {
+		msg := fmt.Sprintf("set controllerReference for Service %s/%s failed", instance.Namespace, instance.Name)
+		logger.Error(err, msg)
+		return nil, err
+	}
+
+	return svc, nil
+}
+
+func (ownService *OwnService) OwnResourceExist(instance *Unit, client client.Client,
+	logger logr.Logger) (bool, interface{}, error) {
+
+	found := &corev1.Service{}
+	err := client.Get(context.TODO(), types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, found)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return false, nil, nil
+		}
+		msg := fmt.Sprintf("Service %s/%s found, but with error", instance.Namespace, instance.Name)
+		logger.Error(err, msg)
+		return true, found, err
+	}
+	return true, found, nil
+}
+
+func (ownService *OwnService) UpdateOwnResourceStatus(instance *Unit, client client.Client, logger logr.Logger) (*Unit, error) {
+
+	found := &corev1.Service{}
+	err := client.Get(context.TODO(), types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, found)
+	if err != nil {
+		return instance, err
+	}
+
+	var portsStatus []ServicePortStatus
+	for _, port := range found.Spec.Ports {
+		health := true
+		chechPort := port.Port
+		addr := found.Spec.ClusterIP
+		sock := fmt.Sprintf("%s:%d", addr, chechPort)
+		proto := string(port.Protocol)
+		_, err := net.DialTimeout(proto, sock, time.Duration(100)*time.Microsecond)
+		if err != nil {
+			health = false
+		}
+		portStatus := ServicePortStatus{
+			ServicePort: port,
+			Health:      health,
+		}
+		portsStatus = append(portsStatus, portStatus)
+	}
+	serviceStatus := UnitRelationServiceStatus{
+		Type:      found.Spec.Type,
+		ClusterIP: found.Spec.ClusterIP,
+		Ports:     portsStatus,
+	}
+	instance.Status.RelationResourceStatus.Service = serviceStatus
+
+	foundEndpoint := &corev1.Endpoints{}
+	err = client.Get(context.TODO(), types.NamespacedName{Namespace: instance.Namespace, Name: instance.Name}, foundEndpoint)
+	if err != nil {
+		return instance, err
+	}
+
+	if foundEndpoint.Subsets != nil && foundEndpoint.Subsets[0].Addresses != nil {
+		var endpointsStatus []UnitRelationEndpointStatus
+		for _, ep := range foundEndpoint.Subsets[0].Addresses {
+			endpointStatus := UnitRelationEndpointStatus{
+				PodName:  ep.Hostname,
+				PodIP:    ep.IP,
+				NodeName: *ep.NodeName,
+			}
+			endpointsStatus = append(endpointsStatus, endpointStatus)
+		}
+		instance.Status.RelationResourceStatus.Endpoint = endpointsStatus
+	}
+	instance.Status.LastUpdateTime = metav1.Now()
+	return instance, nil
+}
+
+// apply this own resource, create or update
+func (ownService *OwnService) ApplyOwnResource(instance *Unit, client client.Client,
+	logger logr.Logger, scheme *runtime.Scheme) error {
+
+	// assert if Service exist
+	exist, found, err := ownService.OwnResourceExist(instance, client, logger)
+	if err != nil {
+		return err
+	}
+
+	// make Service object
+	sts, err := ownService.MakeOwnResource(instance, logger, scheme)
+	if err != nil {
+		return err
+	}
+	newService := sts.(*corev1.Service)
+
+	// apply the Service object just make
+	if !exist {
+		// if Service not exist，then create it
+		msg := fmt.Sprintf("Service %s/%s not found, create it!", newService.Namespace, newService.Name)
+		logger.Info(msg)
+		return client.Create(context.TODO(), newService)
+	} else {
+		foundService := found.(*corev1.Service)
+
+		// 这里有个坑，svc在创建前可能未指定clusterIP，那么svc创建后，会自动指定clusterIP并修改spec.clusterIP字段，因此这里要补上。SessionAffinity同理
+		newService.Spec.ClusterIP = foundService.Spec.ClusterIP
+		newService.Spec.SessionAffinity = foundService.Spec.SessionAffinity
+
+		// if Service exist with change，then try to update it
+		if !reflect.DeepEqual(newService.Spec, foundService.Spec) {
+			msg := fmt.Sprintf("Updating Service %s/%s", newService.Namespace, newService.Name)
+			logger.Info(msg)
+			return client.Update(context.TODO(), newService)
+		}
+		return nil
+	}
 }
--- controllers/unit_controller.go
+++ controllers/unit_controller.go
@@ -18,13 +18,15 @@ package controllers
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	customv1 "kunit/api/v1"
+	"reflect"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	customv1 "kunit/api/v1"
 )
 
 type OwnResource interface {
@@ -49,16 +51,163 @@ type UnitReconciler struct {
 // +kubebuilder:rbac:groups=custom.unit.crd.com,resources=units/status,verbs=get;update;patch
 
 func (r *UnitReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
+	//_ = context.Background()
+	ctx := context.Background()
 	_ = r.Log.WithValues("unit", req.NamespacedName)
 
 	// your logic here
+	defer func() {
+		if rec := recover(); r != nil {
+			switch x := rec.(type) {
+			case error:
+				r.Log.Error(x, "Reconcile error")
+			}
+		}
+	}()
 
-	return ctrl.Result{}, nil
+	// get unit object
+	instance := &customv1.Unit{}
+
+	err := r.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+	}
+	// 2 删除操作
+	// 当资源对象Finalizer字段补位空是，delete操作会变成update操作，即为对象加上deletionTimeStamp时间戳
+	// 当前时间在deletionTimestamp时间之后，且Finalizer已清空（视为清理后续任务处理完成）的情况下，gc会处理此对象
+
+	myFinalizerName := "storage.finalizers.tutorial.kubebuilder.io"
+	// DeletionTimestamp 时间戳为空，代表着当前对象不处于被删除的状态，为了开启Finalizer机制，先给他加上一段Finalizers,内容随机
+	if instance.ObjectMeta.DeletionTimestamp.IsZero() {
+		if !containsString(instance.ObjectMeta.Finalizers, myFinalizerName) {
+			instance.ObjectMeta.Finalizers = append(instance.ObjectMeta.Finalizers, myFinalizerName)
+			if err := r.Update(ctx, instance); err != nil {
+				r.Log.Error(err, "Add Finalizers error", instance.Namespace, instance.Name)
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		// DeletionTimestamp不为空，说明对象已经开始进入删除状态了。执行自己的删除步骤的后续逻辑，并清理掉自己的finalizer字段，等待自动gc
+		if containsString(instance.ObjectMeta.Finalizers, myFinalizerName) {
+			// 执行自定义删除逻辑
+			if err := r.PreDelete(instance); err != nil {
+				return ctrl.Result{}, err
+			}
+			instance.ObjectMeta.Finalizers = removeString(instance.ObjectMeta.Finalizers, myFinalizerName)
+			if err := r.Update(ctx, instance); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// 3 创建或更新操作
+	ownResources, err := r.getOwnResource(instance)
+	if err != nil {
+		msg := fmt.Sprintf("%s %s Reconciler.getOwnResource() function error", instance.Namespace, instance.Name)
+		r.Log.Error(err, msg)
+		return ctrl.Result{}, err
+	}
+
+	// 判断own resource是否存在
+	success := true
+	for _, ownResource := range ownResources {
+		if err = ownResource.ApplyOwnResource(instance, r.Client, r.Log, r.Scheme); err != nil {
+			success = false
+		}
+	}
+
+	// 4 update Unit.status
+	updateInstance := instance.DeepCopy()
+	for _, ownResource := range ownResources {
+		updateInstance, err = ownResource.UpdateOwnResourceStatus(updateInstance, r.Client, r.Log)
+		if err != nil {
+			success = false
+		}
+	}
+	// apply update to apisServer if status changed
+	if updateInstance != nil && !reflect.DeepEqual(updateInstance.Status, instance.Status) {
+		if err := r.Status().Update(context.Background(), updateInstance); err != nil {
+			r.Log.Error(err, "unable to update Unit status")
+		}
+	}
+	// 5 记录结果
+	if !success {
+		msg := fmt.Sprintf("Reconciler Unit %s/%s failed", instance.Namespace, instance.Name)
+		r.Log.Error(err, msg)
+		return ctrl.Result{}, err
+	} else {
+		msg := fmt.Sprintf("Reconciler Unit %s/%s success", instance.Namespace, instance.Name)
+		r.Log.Info(msg)
+		return ctrl.Result{}, nil
+	}
 }
 
 func (r *UnitReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&customv1.Unit{}).
 		Complete(r)
+}
+
+func (r *UnitReconciler) PreDelete(instance *customv1.Unit) error {
+	// 留空，自定义的pre delete逻辑需要，在这里实现
+	return nil
+}
+
+func containsString(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func removeString(slice []string, s string) (result []string) {
+	for _, item := range slice {
+		if item == s {
+			continue
+		}
+		result = append(result, item)
+	}
+	return
+}
+
+// 根据Unit.Spec生成其所有的own resource
+func (r *UnitReconciler) getOwnResource(instance *customv1.Unit) ([]OwnResource, error) {
+	var ownResources []OwnResource
+
+	if instance.Spec.Category == "Deployment" {
+		ownDeployment := customv1.OwnDeployment{
+			Spec: appsv1.DeploymentSpec{
+				Replicas: instance.Spec.Replicas,
+				Selector: instance.Spec.Selector,
+				Template: instance.Spec.Template,
+			},
+		}
+		ownDeployment.Spec.Template.Labels = instance.Spec.Selector.MatchLabels
+		ownResources = append(ownResources, &ownDeployment)
+	} else {
+		ownStatefulSet := &customv1.OwnStatefulSet{
+			Spec: appsv1.StatefulSetSpec{
+				Replicas:    instance.Spec.Replicas,
+				Selector:    instance.Spec.Selector,
+				Template:    instance.Spec.Template,
+				ServiceName: instance.Name,
+			},
+		}
+		ownResources = append(ownResources, ownStatefulSet)
+	}
+	if instance.Spec.RelationResource.Service != nil {
+		ownResources = append(ownResources, instance.Spec.RelationResource.Service)
+	}
+	if instance.Spec.RelationResource.Ingress != nil {
+		ownResources = append(ownResources, instance.Spec.RelationResource.Ingress)
+	}
+	if instance.Spec.RelationResource.PVC != nil {
+		ownResources = append(ownResources, instance.Spec.RelationResource.PVC)
+	}
+	return ownResources, nil
 }
